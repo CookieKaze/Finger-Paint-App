e712f3b0be4fe4f98e86a0ec925a93311319d6f7
Erin Luu <luuerin@gmail.com>
2016-11-11 13:17:03 -0500

    Initial Commit

---
+++ Finger Paint App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Finger Paint App
+//
+//  Created by Erin Luu on 2016-11-11.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
